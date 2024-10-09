91c66c726dcc0efd05aabb5f7ecd0bb3d42841f4
Brian Simiyu <briansimiyuj@gmail.com>
2024-10-09 14:32:15 +0300

    Creating a server

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ We'll create a server using a custom event emitter.
 1. Import the events module
 2. Create an instance of the EventEmitter class
 3. Create an event handler
+4. Import the http module
+5. Create a server
+    a. Set up request and response objects
+    b. Send a response
--- script.js
+++ script.js
@@ -1,9 +1,28 @@
 import EventEmitter from "events"
+import http from "http"
 
 const myEmitter = new EventEmitter()
 
 myEmitter.on("creatingServer", (port, host) =>{
 
+    const server = http.createServer((request, response) =>{
 
-    
+        console.log('Server is running' + request.url)
+
+        response.writeHead(
+
+            200,
+
+            {
+                "Content-Type": "text/plain"
+            }
+
+        )
+
+        response.end('My first Node.js server')
+
+    })
+
+    server.listen(port, host, () => console.log('Server is running'))
+
 })
